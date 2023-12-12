eefdd8a14ef90e20e3eae388142e2daac3bc146e
DevWedeloper <vicnathangabrielle@gmail.com>
2023-12-13 00:06:37 +0800

    refactor: remove duplicate code and some blank lines

--- functions/src/controllers/loginController.ts
+++ functions/src/controllers/loginController.ts
@@ -4,15 +4,13 @@ import jwt, { JwtPayload } from 'jsonwebtoken';
 import * as RefreshToken from '../models/refreshAccessTokenModel';
 import * as User from '../models/userModel';
 
-const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET as string;
-const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET as string;
-
 export const login = async (
   req: Request,
   res: Response
 ): Promise<void | Response> => {
-  const { username, password } = req.body;
   try {
+    const { username, password } = req.body;
+    
     const user = await User.findOneByQuery({ username });
     if (!user || !bcrypt.compareSync(password, user.password)) {
       return res.status(401).json({
@@ -21,15 +19,17 @@ export const login = async (
       });
     }
 
+    const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET as string;
+    const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET as string;
+    const expiresInDays = process.env.REFRESH_TOKEN_EXPIRATION!;
+    const expiresAt = new Date(
+      Date.now() + parseInt(expiresInDays) * 24 * 60 * 60 * 1000
+    );
+
     let refreshTokenEntry = await RefreshToken.isUnique({
       userId: user._id.toString(),
     });
     if (!refreshTokenEntry) {
-      const expiresInDays = process.env.REFRESH_TOKEN_EXPIRATION!;
-      const expiresAt = new Date(
-        Date.now() + parseInt(expiresInDays) * 24 * 60 * 60 * 1000
-      );
-
       const refreshToken = jwt.sign(
         { userId: user._id, username: user.username, role: user.role },
         refreshTokenSecret,
@@ -48,13 +48,7 @@ export const login = async (
 
     const refreshTokenData = jwt.decode(refreshTokenEntry.token) as JwtPayload;
     const currentTime = Math.floor(Date.now() / 1000);
-
     if (refreshTokenData.exp! < currentTime) {
-      const expiresInDays = process.env.REFRESH_TOKEN_EXPIRATION!;
-      const expiresAt = new Date(
-        Date.now() + parseInt(expiresInDays) * 24 * 60 * 60 * 1000
-      );
-
       const newRefreshToken = jwt.sign(
         { userId: user._id, username: user.username, role: user.role },
         refreshTokenSecret,
@@ -74,7 +68,6 @@ export const login = async (
       accessTokenSecret,
       { expiresIn: `${process.env.ACCESS_TOKEN_EXPIRATION}` }
     );
-
     return res.status(201).json({
       userId: user._id,
       accessToken,
