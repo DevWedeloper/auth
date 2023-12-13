ff2a25528cfb13db18dd99ed5eb525b21eefb909
DevWedeloper <vicnathangabrielle@gmail.com>
2023-12-14 02:26:34 +0800

    refactor: make corsConfig file, use it in index via import

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,27 +7,11 @@ import { connect } from 'mongoose';
 dotenv.config();
 
 // import { refreshTokenCleanup } from './cron/refreshTokenCleanup';
+import { corsOptions } from './corsConfig';
 import loginRoute from './routes/loginRoute';
 import userRouter from './routes/userRoute';
 
 const app: Application = express();
-
-const allowedOrigins = [
-  process.env.ALLOWED_ORIGIN,
-  process.env.ALLOWED_ORIGIN_VERCEL,
-];
-const corsOptions = {
-  origin: (
-    origin: string | undefined,
-    callback: (arg0: Error | null, arg1: boolean | undefined) => void
-  ) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'), false);
-    }
-  },
-};
 
 app.use(cors(corsOptions));
 
@@ -40,15 +24,15 @@ const connectToDatabase = async () => {
   }
 };
 
-// connectToDatabase();
+connectToDatabase();
 app.use(express.json());
 
 app.use('/user', userRouter);
 app.use('/', loginRoute);
 
-// app.listen(process.env.MY_PORT, () => {
-//   console.log(`Server is running on PORT ${process.env.MY_PORT}`);
-// });
+app.listen(process.env.MY_PORT, () => {
+  console.log(`Server is running on PORT ${process.env.MY_PORT}`);
+});
 // cron.schedule('0 0 * * *', refreshTokenCleanup);
 
 export const api = functions.https.onRequest((connectToDatabase(), app));
